07af53d999c67b26d70a172ee5cde1950fd017d4
gdkvntn <73638301+gdkvntn@users.noreply.github.com>
2024-08-22 17:37:20 +0300

    fix select

--- front/src/components/Card.js
+++ front/src/components/Card.js
@@ -6,7 +6,6 @@ export const Card = ({ name, handleSelectSign, sign, isLanguage }) => {
   const [select, setSelect] = useState("today");
 
   const handleSelectCLick = (e) => {
-    e.stopPropagation();
     setSelect(e.target.value);
   };
 
@@ -19,7 +18,8 @@ export const Card = ({ name, handleSelectSign, sign, isLanguage }) => {
       <ZadiacIcon width={40} height={40} sign={sign} />
       <h2 className=" text-xl mb-2 mt-2">{name}</h2>
       <select
-        onClick={handleSelectCLick}
+        onClick={(e) => e.stopPropagation()}
+        onChange={handleSelectCLick}
         className="p-1 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
       >
         <option value="today">{zadiacLang[isLanguage]["today"]}</option>
